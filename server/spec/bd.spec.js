29759edc0a99a38ceb3f0beebdfad7394b596b15
JoaoGMSF <jgmsf@cin.ufpe.br>
2022-10-18 12:13:07 -0300

    testes unitários de servidor de banco de dados implementados

--- server/spec/bd.spec.js
+++ server/spec/bd.spec.js
@@ -1,9 +1,11 @@
 import { createPlaylist, deletePlaylist, downloadPlaylist, getPlaylist, updatePlaylist } from '../src/infrastructure/internal/playlist.js'
+import { uploadMusic, deleteMusic, getMusic } from '../src/infrastructure/internal/music.js'
+import { readFileSync, writeFileSync } from 'fs'
 
 describe("O uso do banco de dados", () => {
     var originalTimeout;
     let newPlaylist = {
-        id: 9,
+        id: 0,
         name: "Rap Naruto",
         image: "",
         accountID: 1,
@@ -13,7 +15,7 @@ describe("O uso do banco de dados", () => {
         category: "Rap"
     };
     let updatedPlaylist = {
-        id: 9,
+        id: 0,
         name: "Openings One Piece",
         image: "",
         accountID: 1,
@@ -23,7 +25,7 @@ describe("O uso do banco de dados", () => {
         category: "Rap"
     };
     let existentPlaylist = {
-        id:8,
+        id:34,
         name:"pipipi popopo",
         image:"/images/youtube.png",
         accountID:1,
@@ -31,6 +33,26 @@ describe("O uso do banco de dados", () => {
         followers:[],
         relevance:0,
         category:"teste 2"
+    };
+    let newMusic = {
+        id: undefined,
+        image:"https://projeto-ess-sharkmusic-bucket.s3.sa-east-1.amazonaws.com/tobi.jpg",
+        name:"tipo madara",
+        owner:"Jonga Doido",
+        album:"MHRAP",
+        releaseDate:"21 de jul. de 1945",
+        duration:"3:25"
+    };
+    let updatedMusic = {
+        id: undefined,
+        image:"https://projeto-ess-sharkmusic-bucket.s3.sa-east-1.amazonaws.com/tobi.jpg",
+        name:"tipo madara",
+        owner:"Jonga Doido",
+        album:"MHRAP",
+        releaseDate:"21 de jul. de 1945",
+        duration:"3:25",
+        accountID:[2,3],
+        url: ''
     };
 
     beforeAll(() => {
@@ -50,29 +72,100 @@ describe("O uso do banco de dados", () => {
         console.log('\n');
       });
     
+      function writeDB(tableName,updatedTable) {
+        let data = readFileSync('./src/infrastructure/internal/database/database.json')
+        let db = JSON.parse(data)
+        db[tableName] = updatedTable
+        writeFileSync('./src/infrastructure/internal/database/database.json', JSON.stringify(db))
+    }
+
+
+    function readDB(table) {
+        let data = readFileSync('./src/infrastructure/internal/database/database.json')
+        let db = JSON.parse(data)
+        return db[table]
+    }
+
+
+    function readPlaylists(){
+        return readDB('playlists')
+    }
+
+    function readMusics(){
+        return readDB('musics')
+    }
+
+
+    function nextInt(table) {
+
+        let maxIndex = 1
+        if (table.length === 0 || (table.length === 1 && table[0] === null) ) return 1 
+        for (let i = 0; i<table.length; i = i+1) {
+            if (maxIndex <= table[i].id) {
+                maxIndex = table[i].id + 1
+            }
+        }
+        return maxIndex
+    }
+
+    function nextIntPlaylists(){
+        return nextInt(readPlaylists());
+    }
+
+    function nextIntMusics(){
+        return nextInt(readMusics());
+    }
+      
+
     it("testando getPlaylist", () => {
-          expect(getPlaylist(8)).toEqual(existentPlaylist);
+          expect(getPlaylist(nextIntPlaylists()-1)).toEqual(readPlaylists()[readPlaylists().length-1]);
     })    
 
     it("testando createPlaylist", () => {
         createPlaylist(newPlaylist.name, newPlaylist.image, newPlaylist.accountID, newPlaylist.category)
-        expect(getPlaylist(newPlaylist.id)).toEqual(newPlaylist);
-        deletePlaylist(newPlaylist.id);
+        var proxInt = nextIntPlaylists()-1;
+        expect(getPlaylist(proxInt)).toEqual(readPlaylists()[readPlaylists().length-1]);
+        deletePlaylist(proxInt);
     })    
 
     it("testando deletePlaylist", () => {
         createPlaylist(newPlaylist.name, newPlaylist.image, newPlaylist.accountID, newPlaylist.category)
-        expect(getPlaylist(newPlaylist.id)).toEqual(newPlaylist);
-        deletePlaylist(newPlaylist.id);
-        expect(getPlaylist(newPlaylist.id)).toBe(null)
+        var proxInt = nextIntPlaylists()-1;
+        newPlaylist.id = proxInt;
+        expect(getPlaylist(proxInt)).toEqual(newPlaylist);
+        deletePlaylist(proxInt);
+        expect(getPlaylist(proxInt)).toBe(null)
     })    
 
     it("testing updatePlaylist", () => {
         createPlaylist(newPlaylist.name, newPlaylist.image, newPlaylist.accountID, newPlaylist.category)
         newPlaylist.name = "Openings One Piece";
+        var proxInt = nextIntPlaylists()-1;
+        newPlaylist.id = proxInt;
+        updatedPlaylist.id = proxInt;
         updatePlaylist(newPlaylist)
-        expect(getPlaylist(newPlaylist.id)).toEqual(updatedPlaylist);
-        deletePlaylist(newPlaylist.id);
+        expect(getPlaylist(proxInt)).toEqual(updatedPlaylist);
+        deletePlaylist(proxInt);
     })    
+
+    it("testing uploadMusic", () => {
+        uploadMusic(newMusic.image, newMusic.name, newMusic.owner, newMusic.album, newMusic.releaseDate, newMusic.duration, [2, 3], '')
+        var proxInt = nextIntMusics()-1;
+        expect(getMusic(proxInt)).toEqual(readMusics()[readMusics().length-1]);
+        deleteMusic(proxInt);
+    })   
+    
+    it("testing getMusic", () => {
+        expect(getMusic(nextIntMusics()-1)).toEqual(readMusics()[readMusics().length-1]);
+  })    
+
+  it("testando deleteMusic", () => {
+    uploadMusic(newMusic.image, newMusic.name, newMusic.owner, newMusic.album, newMusic.releaseDate, newMusic.duration, undefined, undefined)
+    var proxInt = nextIntMusics()-1;
+    newMusic.id = proxInt;
+    expect(getMusic(proxInt)).toEqual(newMusic);
+    deleteMusic(proxInt);
+    expect(readMusics()[proxInt]).toEqual(undefined)
+})    
 
   })
--- server/spec/playlists.spec.js
+++ server/spec/playlists.spec.js
@@ -2,7 +2,7 @@ import { CreatePlaylistUseCase, DeletePlaylistUseCase, UpdatePlaylistUseCase, Ge
 import { CreatePlaylistUseCaseRequest, DeletePlaylistUseCaseRequest, UpdatePlaylistUseCaseRequest, GetPlaylistUseCaseRequest } from '../src/domain/ucio/playlist.js';
 import { CreatePlaylistUseCaseValidate, DeletePlaylistUseCaseValidate, UpdatePlaylistUseCaseValidate, GetPlaylistUseCaseValidate } from '../src/infrastructure/provider/validate/playlist.js';
 import { CreatePlaylistUseCaseRepository, DeletePlaylistUseCaseRepository, UpdatePlaylistUseCaseRepository, GetPlaylistUseCaseRepository } from '../src/infrastructure/provider/repository/playlist.js';
-
+import { readFileSync, writeFileSync } from 'fs'
 
 describe("O uso de playlist", () => {
     var originalTimeout;
@@ -12,7 +12,7 @@ describe("O uso de playlist", () => {
     var repository;
     var response;
     let newPlaylist = {
-        id: 9,
+        id: 35,
         name: "Rap Naruto",
         image: "",
         accountID: 1,
@@ -20,7 +20,7 @@ describe("O uso de playlist", () => {
         musics: ["Rap da Akatsuki", "Essa dor que eu causei"]
     };
     let wrongPlaylist = {
-        id: 9,
+        id: 35,
         name: "",
         image: "",
         accountID: 1,
@@ -44,7 +44,42 @@ describe("O uso de playlist", () => {
     afterAll(() => {
         console.log('\n');
       });
+      
+    function writeDB(tableName,updatedTable) {
+        let data = readFileSync('./src/infrastructure/internal/database/database.json')
+        let db = JSON.parse(data)
+        db[tableName] = updatedTable
+        writeFileSync('./src/infrastructure/internal/database/database.json', JSON.stringify(db))
+    }
     
+
+    function readDB(table) {
+        let data = readFileSync('./src/infrastructure/internal/database/database.json')
+        let db = JSON.parse(data)
+        return db[table]
+    }
+
+
+    function readPlaylists(){
+        return readDB('playlists')
+    }
+
+    function nextInt(table) {
+
+        let maxIndex = 1
+        if (table.length === 0 || (table.length === 1 && table[0] === null) ) return 1 
+        for (let i = 0; i<table.length; i = i+1) {
+            if (maxIndex <= table[i].id) {
+                maxIndex = table[i].id + 1
+            }
+        }
+        return maxIndex
+    }
+
+    function nextIntPlaylists(){
+        return nextInt(readPlaylists());
+    }
+      
     function createPlaylist(){
         ucReq = new CreatePlaylistUseCaseRequest(newPlaylist.name, newPlaylist.image, newPlaylist.accountID,newPlaylist.category);    
         validate = new CreatePlaylistUseCaseValidate();
@@ -55,7 +90,7 @@ describe("O uso de playlist", () => {
     }
 
     function deletePlaylist(){
-        ucReq = new DeletePlaylistUseCaseRequest(newPlaylist.id);    
+        ucReq = new DeletePlaylistUseCaseRequest(nextIntPlaylists()-1);    
         validate = new DeletePlaylistUseCaseValidate();
         repository = new DeletePlaylistUseCaseRepository();
         usecase = new DeletePlaylistUseCase(validate, repository);
@@ -87,7 +122,7 @@ describe("O uso de playlist", () => {
 
     it("validate updatePlaylists validando playlist correta", () => {
         createPlaylist();
-        ucReq = new UpdatePlaylistUseCaseRequest(newPlaylist.id, newPlaylist.name, newPlaylist.image, newPlaylist.category, newPlaylist.musics, newPlaylist.accountID);    
+        ucReq = new UpdatePlaylistUseCaseRequest((nextIntPlaylists()-1), newPlaylist.name, newPlaylist.image, newPlaylist.category, newPlaylist.musics, newPlaylist.accountID);    
         validate = new UpdatePlaylistUseCaseValidate();
         expect(validate.updatePlaylist(ucReq)).toBe(null);
         deletePlaylist();
@@ -95,7 +130,7 @@ describe("O uso de playlist", () => {
   
     it("validate updatePlaylists validando playlist sem nome", () => {
         createPlaylist();
-        ucReq = new UpdatePlaylistUseCaseRequest(wrongPlaylist.id, wrongPlaylist.name, "", wrongPlaylist.category, wrongPlaylist.musics, wrongPlaylist.accountID);    
+        ucReq = new UpdatePlaylistUseCaseRequest((nextIntPlaylists()-1), wrongPlaylist.name, "", wrongPlaylist.category, wrongPlaylist.musics, wrongPlaylist.accountID);    
         validate = new UpdatePlaylistUseCaseValidate();
         expect(validate.updatePlaylist(ucReq)).toBe('O nome da playlist não pode ser vazio.');
         deletePlaylist();
@@ -103,7 +138,7 @@ describe("O uso de playlist", () => {
 
     it("atualização de uma playlist", () => {
         createPlaylist();
-        ucReq = new UpdatePlaylistUseCaseRequest(newPlaylist.id, newPlaylist.name, "", newPlaylist.category, newPlaylist.musics, newPlaylist.accountID);    
+        ucReq = new UpdatePlaylistUseCaseRequest((nextIntPlaylists()-1), newPlaylist.name, "", newPlaylist.category, newPlaylist.musics, newPlaylist.accountID);    
         validate = new UpdatePlaylistUseCaseValidate();
         repository = new UpdatePlaylistUseCaseRepository();
         usecase = new UpdatePlaylistUseCase(validate, repository);
@@ -112,11 +147,34 @@ describe("O uso de playlist", () => {
         deletePlaylist();
     })
 
+
+    it("atualização de uma playlist adiconando músicas", () => {
+        createPlaylist();
+        ucReq = new UpdatePlaylistUseCaseRequest((nextIntPlaylists()-1), newPlaylist.name, "", newPlaylist.category, ["ninja mais forte", "rap dos hokages"], newPlaylist.accountID);    
+        validate = new UpdatePlaylistUseCaseValidate();
+        repository = new UpdatePlaylistUseCaseRepository();
+        usecase = new UpdatePlaylistUseCase(validate, repository);
+        response = usecase.updatePlaylist(ucReq);
+        expect(response.playlist.musics).toEqual(["ninja mais forte", "rap dos hokages"]);
+        deletePlaylist();
+    })
+
+    it("atualização de uma playlist removendo músicas", () => {
+        createPlaylist();
+        ucReq = new UpdatePlaylistUseCaseRequest((nextIntPlaylists()-1), newPlaylist.name, "", newPlaylist.category, [], newPlaylist.accountID);    
+        validate = new UpdatePlaylistUseCaseValidate();
+        repository = new UpdatePlaylistUseCaseRepository();
+        usecase = new UpdatePlaylistUseCase(validate, repository);
+        response = usecase.updatePlaylist(ucReq);
+        expect(response.playlist.musics).toEqual([]);
+        deletePlaylist();
+    })
+
     //TESTES DELETANDO PLAYLIST
 
     it("validate deletePlaylist validando playlist correta", () => {
         createPlaylist();
-        ucReq = new DeletePlaylistUseCaseRequest(newPlaylist.id);    
+        ucReq = new DeletePlaylistUseCaseRequest(nextIntPlaylists()-1);    
         validate = new DeletePlaylistUseCaseValidate();
         expect(validate.deletePlaylist(ucReq)).toBe(null);
         deletePlaylist();
@@ -139,7 +197,7 @@ describe("O uso de playlist", () => {
 
     it("validate getPlaylist validando playlist correta", () => {
         createPlaylist();
-        ucReq = new GetPlaylistUseCaseRequest(newPlaylist.id);    
+        ucReq = new GetPlaylistUseCaseRequest((nextIntPlaylists()-1));    
         validate = new GetPlaylistUseCaseValidate();
         expect(validate.getPlaylist(ucReq)).toBe(null);
         deletePlaylist();
@@ -155,7 +213,7 @@ describe("O uso de playlist", () => {
 
     it("retornando playlist", () => {
         createPlaylist();
-        ucReq = new GetPlaylistUseCaseRequest(newPlaylist.id);    
+        ucReq = new GetPlaylistUseCaseRequest((nextIntPlaylists()-1));    
         validate = new GetPlaylistUseCaseValidate();
         repository = new GetPlaylistUseCaseRepository();
         usecase = new GetPlaylistUseCase(validate, repository);
