a40a54328c393798109880d6565cc73956032fda
JP2821 <jprsd@cin.ufpe.br>
2022-10-17 23:02:53 -0300

    pagina de categorias

---
+++ gui/src/components/Categorieslist/CategoriesList.jsx
@@ -0,0 +1,36 @@
+import { Route, BrowserRouter, Routes} from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar/navbar.jsx';
+import Player from './components/Player/Player';
+import Playlist from './components/PlaylistPage/PlaylistPage';
+import ShowList from './components/ShowList/ShowList.jsx';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import Carousel, { CarouselItem } from './components/carousel/Carousel';
+
+library.add(fab,fas)
+
+function CategoriesList() {
+
+  localStorage.setItem('accountID', 1)
+
+  return (
+
+    <div>
+      <Navbar/>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            element={<ShowList></ShowList>}
+            path="/"
+          ></Route>
+          <Route element={<Playlist></Playlist>} path="/PlaylistPage"></Route>
+        </Routes>
+      </BrowserRouter>
+      <Player/>
+    </div>
+  );
+}
+
+export default CategoriesList;
--- gui/src/components/Navbar/navbar.jsx
+++ gui/src/components/Navbar/navbar.jsx
@@ -4,8 +4,24 @@ import'./NavBar.modules.css'
 import logo from '../../img/image 2.svg'
 import Home from '../../img/Home.svg'
 import Library from '../../img/Library.svg'
+import { useEffect } from 'react'
+import axiosInstance from '../common/server'
 
 function NavBar(){
+//     useEffect((axiosInstance)=>{
+//         async function fetchCategories() {
+//             const response = await axiosInstance({
+//               method: "post",
+//               url: `/getUser`,
+//               headers: {},
+//               data: {
+//                 category: parseInt(localStorage.getItem(''),10)
+//               },
+//             });
+//             let val = await response.data
+//             return val
+//         }
+//     })
     return(
         <nav className='navbar'> 
             <Container>
@@ -30,7 +46,9 @@ function NavBar(){
                             </a>
                         </li>
                         <li className="LibraryAdjust">
-                        <img className="LibraryAdjustImage" src={Library} alt="" />
+                            <a href='/listPlaylistByCategory'>
+                                <img className="LibraryAdjustImage" src={Library} alt="" />
+                            </a>
                         </li>
                         <li className="item">
                             <input className='lupa' type="text" name='Artists, songs, or podcasts' placeholder='Artists, songs, or podcasts' />
